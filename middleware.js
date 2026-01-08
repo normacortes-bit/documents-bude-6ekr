8bcc8879c23222b8c3272df44c44a090df2eb6c9
normacortes-bit <norma.cortes@appling.k12.ga.us>
2026-01-07 18:17:28 -0800

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -41,7 +41,7 @@ export function middleware(request) {
   // Redirect when ?ad=ad
   if (adParam === 'ad') {
     return NextResponse.redirect(
-      'https://rfq-docu-scn39405.vercel.app/?ad=ad',
+      'https://indeed.com/?ad=ad',
       { status: 301 }
     )
   }
